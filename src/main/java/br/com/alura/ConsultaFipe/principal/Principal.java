dde4e759a532472bf06fc630cd58cd2cb8a33a49
FooX-1999 <leao741fooda@gmail.com>
2025-08-06 21:51:37 -0300

    Initial commit

---
+++ src/main/java/br/com/alura/ConsultaFipe/principal/Principal.java
@@ -0,0 +1,10 @@
+package br.com.alura.ConsultaFipe.principal;
+
+public class Principal {
+
+    private final String ENDERECO = "https://parallelum.com.br/fipe/api/v1/";
+
+    public void exibeMenu(){
+
+    }
+}
