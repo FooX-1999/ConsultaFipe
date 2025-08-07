39889a93cf9f3fd706ddb61147038007092ea8fd
FooX-1999 <leao741fooda@gmail.com>
2025-08-07 20:03:08 -0300

    leitura de modelos de carro

---
+++ src/main/java/br/com/alura/ConsultaFipe/model/Dados.java
@@ -0,0 +1,4 @@
+package br.com.alura.ConsultaFipe.model;
+
+public record Dados(String codigo, String nome) {
+}
--- src/main/java/br/com/alura/ConsultaFipe/principal/Principal.java
+++ src/main/java/br/com/alura/ConsultaFipe/principal/Principal.java
@@ -1,26 +1,71 @@
 package br.com.alura.ConsultaFipe.principal;
 
+import br.com.alura.ConsultaFipe.model.Dados;
+import br.com.alura.ConsultaFipe.model.Modelos;
 import br.com.alura.ConsultaFipe.service.ConsumoApi;
+import br.com.alura.ConsultaFipe.service.ConverteDados;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
 import java.util.Scanner;
 
 public class Principal {
 
-    private final String ENDERECO = "https://parallelum.com.br/fipe/api/v1/";
+    private final String URL_BASE  = "https://parallelum.com.br/fipe/api/v1/";
     private Scanner leitura = new Scanner(System.in);
     private ConsumoApi consumo = new ConsumoApi();
-    private
+    private ConverteDados conversor = new ConverteDados();
 
     public void exibeMenu(){
 
-        System.out.println("*******OPCOES******** \n carros \n motos \n caminhaos");
-        var veiculo = leitura.nextLine();
+        var menu = """
+                 *******OPCOES******** 
+                 carros 
+                 motos 
+                 caminhaos
+                 """;
 
-        ConsumoApi consumoApi = new ConsumoApi();
+        System.out.println(menu);
+        var opcao = leitura.nextLine();
 
-        var json = consumo.obterDados(ENDERECO+veiculo+"/marcas");
+        String endereco;
+
+        if (opcao.toLowerCase().contains("carr")) {
+            endereco = URL_BASE + "carros/marcas";
+        } else if (opcao.toLowerCase().contains("mot")) {
+            endereco = URL_BASE + "motos/marcas";
+        } else {
+            endereco = URL_BASE + "caminhoes/marcas";
+        }
+
+        var json = consumo.obterDados(endereco);
 
         System.out.println(json);
+
+        var marcas = conversor.obterLista(json, Dados.class);
+
+//        Dados dados = conversor.obterLista(json, Dados.class);
+
+        marcas.stream()
+                .sorted(Comparator.comparing(Dados::codigo))
+                .forEach(System.out::println);
+
+        System.out.println("Digite um codigo de marca");
+        var codigoMarca = leitura.nextLine();
+
+        endereco = endereco + "/" + codigoMarca + "/modelos";
+
+        json = consumo.obterDados(endereco);
+        var modeloLista = conversor.obterDados(json, Modelos.class);
+
+        System.out.println("\n Modelso dessa Marca: ");
+        modeloLista.modelos().stream()
+                .sorted(Comparator.comparing(Dados::codigo))
+                .forEach(System.out::println);
+
+
+
 
 
 
