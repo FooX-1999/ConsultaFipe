420d62b14e821d3e43c41192ec528f7040e136b3
FooX-1999 <leao741fooda@gmail.com>
2025-08-06 22:48:02 -0300

    leitura de api ta feita

--- src/main/java/br/com/alura/ConsultaFipe/principal/Principal.java
+++ src/main/java/br/com/alura/ConsultaFipe/principal/Principal.java
@@ -1,10 +1,29 @@
 package br.com.alura.ConsultaFipe.principal;
 
+import br.com.alura.ConsultaFipe.service.ConsumoApi;
+
+import java.util.Scanner;
+
 public class Principal {
 
     private final String ENDERECO = "https://parallelum.com.br/fipe/api/v1/";
+    private Scanner leitura = new Scanner(System.in);
+    private ConsumoApi consumo = new ConsumoApi();
+    private
 
     public void exibeMenu(){
+
+        System.out.println("*******OPCOES******** \n carros \n motos \n caminhaos");
+        var veiculo = leitura.nextLine();
+
+        ConsumoApi consumoApi = new ConsumoApi();
+
+        var json = consumo.obterDados(ENDERECO+veiculo+"/marcas");
+
+        System.out.println(json);
+
+
+
 
     }
 }
